f0f19026c4f3877131e0a6ac3a64a384608d1657
Alex <jvaughn1230@gmail.com>
2023-04-05 09:52:28 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Hero from "./containers/Hero";
+import About from "./containers/About";
+import Skills from "./containers/Skills";
+import Projects from "./containers/Projects";
+import Contact from "./containers/Contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
